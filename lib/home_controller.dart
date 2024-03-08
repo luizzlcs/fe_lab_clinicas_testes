f33a488b46ab288b664bb70eaa8896f63e9e4c08
Luizzlcs <luizzlcs@gmail.com>
2024-03-08 12:46:34 -0300

    Projeto academia do flutter

---
+++ lib/home_controller.dart
@@ -0,0 +1,8 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+
+class HomeController with MessageStateMixin {
+
+  void fazerAlgo(){
+    showError('Erro ao fazer algo');
+  }
+}
